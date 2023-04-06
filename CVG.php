f11786ad8d0fb929fadfd38dffec69259ce32779
nebula <nebula@NEBULABOT>
2023-04-06 23:51:10 +0200

    Init commit

---
+++ CVG.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CONDITIONS GÉNÉRALES DE VENTE - Solescape</title>
+    <link rel="stylesheet" href="CSS/style.css">
+    <link rel="icon" href="assets/image_st/Logo_Solescape.png" type="image/png" sizes="16x16">
+</head>
+<body>
+    <?php
+        include 'header.html';
+    ?>
+
+    <div class="container-CVG">
+        <h2>Conditions Générales de Vente</h2>
+        <p><strong>1. Objet</strong></p>
+        <p>Les présentes conditions générales de vente (CGV) ont pour objet de définir les modalités de vente et de livraison des produits proposés par Solescape, ainsi que les droits et obligations des parties dans le cadre de la vente en ligne de sneakers.</p>
+        
+        <p><strong>2. Produits</strong></p>
+        <p>Les produits proposés à la vente sur le site Solescape sont des sneakers de marques variées et de différentes tailles. Les produits sont décrits avec précision sur le site et peuvent faire l'objet de photos ou de vidéos pour illustrer leur qualité et leur état.</p>
+        
+        <p><strong>3. Prix et paiement</strong></p>
+        <p>Les prix affichés sur le site Solescape sont en euros, toutes taxes comprises (TTC). Les frais de livraison sont indiqués en sus du prix de vente. Les paiements sont effectués en ligne au moment de la commande, par carte bancaire ou par tout autre moyen de paiement proposé par le site.</p>
+        
+        <p><strong>4. Commande</strong></p>
+        <p>Les commandes passées sur le site Solescape sont réputées fermes et définitives après validation du paiement. Le site se réserve le droit de refuser toute commande d'un client avec lequel un litige précédent existerait.</p>
+        
+        <p><strong>5. Livraison</strong></p>
+        <p>Les produits sont livrés à l'adresse indiquée par le client lors de la commande. Les délais de livraison peuvent varier en fonction de la disponibilité des produits et des conditions de livraison. Solescape s'engage à tout mettre en œuvre pour que les produits soient livrés dans les meilleurs délais.</p>
+        
+        <p><strong>6. Droit de rétractation</strong></p>
+        <p>Le client dispose d'un délai de quatorze (14) jours pour exercer son droit de rétractation, à compter de la réception des produits. Le produit devra être retourné à Solescape dans son emballage d'origine, en parfait état et accompagné de tous les accessoires éventuels. Les frais de retour sont à la charge du client.</p>
+        
+        <p><strong>7. Garantie</strong></p>
+        <p>Les produits vendus sur le site Solescape sont garantis contre tout défaut de fabrication pendant une période de six (6) mois à compter de la date de livraison. La garantie ne couvre pas les dommages causés par une utilisation inappropriée ou une usure normale du produit.</p>
+        
+        <p><strong>8. Données personnelles</strong></p>
+        <p>Les informations collectées lors de la commande sont nécessaires à la bonne exécution des opérations de vente et de livraison. Les données personnelles du client sont traitées conformément à la loi Informatique et Libertés du 6 janvier 1978. Le client dispose d'un droit d'accès, de rectification et d'opposition aux données le concernant, qu'il peut exercer en contactant Solescape.</p>
+    </div>
+
+    <?php
+        include 'footer.html';
+    ?>
+
+</body>
