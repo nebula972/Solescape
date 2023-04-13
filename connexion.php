21a678f4424d9a7a7a03c7adb481278537404f9b
nebula <nebula@NEBULABOT>
2023-04-13 19:03:04 +0200

    debut javascript pour panier et fav, maquette snk fonctionne

---
+++ connexion.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>connexion - Solescape</title>
+    <link rel="stylesheet" href="CSS/style.css">
+    <link rel="icon" href="assets/image_st/Logo_Solescape.png" type="image/png" sizes="16x16">
+</head>
+<body>
+    
+</body>
+</html>
