7b727e4c7c122de163014de63d9da4f0b35b9176
nebula <nebula@NEBULABOT>
2023-05-14 02:00:01 +0200

    FIN

---
+++ Pdc.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CONDITIONS GÉNÉRALES DE VENTE - Solescape</title>
+    <link rel="stylesheet" href="CSS/style.css">
+    <link rel="icon" href="assets/image_st/Logo_Solescape.png" type="image/png" sizes="16x16">
+</head>
+<body>
+    <?php
+        include 'header.html';
+    ?>
+
+    <div class="container-CVG">
+    <h2>Politique de confidentialité de Solescape</h2><br><br>
+
+    <h3>Dernière mise à jour : 20/04/2023</h3><br><br>
+
+    <h4>Introduction</h4> <br>
+    <p>Solescape est un site web qui propose des produits et des services liés aux voyages. Nous nous engageons à protéger la vie privée de nos utilisateurs. Cette politique de confidentialité explique comment nous collectons, utilisons, divulguons et protégeons les informations personnelles des utilisateurs de Solescape.
+
+    Collecte d'informations
+    Nous collectons plusieurs types d'informations lorsque vous utilisez notre site web, notamment :
+
+    Informations personnelles : Nous pouvons collecter des informations telles que votre nom, votre adresse e-mail, votre adresse postale, votre numéro de téléphone et d'autres informations similaires lorsque vous vous inscrivez à notre newsletter, créez un compte, effectuez un achat ou nous contactez.
+
+    Informations de navigation : Nous pouvons collecter des informations sur votre utilisation de notre site web, telles que les pages que vous visitez, le temps passé sur le site et d'autres informations similaires.
+
+    Utilisation des informations
+    Nous utilisons les informations que nous collectons pour :
+
+    Fournir des produits et des services : Nous utilisons vos informations personnelles pour traiter vos commandes, répondre à vos demandes et vous fournir des produits et des services.
+
+    Personnaliser l'expérience utilisateur : Nous utilisons les informations que nous collectons pour améliorer votre expérience sur notre site web.
+
+    Communication : Nous utilisons vos informations personnelles pour communiquer avec vous, notamment pour vous informer des promotions, des nouvelles offres et des événements spéciaux.
+
+    Divulgation d'informations
+    Nous ne vendons ni ne louons vos informations personnelles à des tiers. Cependant, nous pouvons divulguer vos informations personnelles dans les cas suivants :
+
+    Partenaires de confiance : Nous pouvons partager vos informations personnelles avec des partenaires de confiance qui travaillent avec nous pour fournir des produits et des services à nos utilisateurs.
+
+    Conformité à la loi : Nous pouvons divulguer vos informations personnelles si la loi l'exige ou si nous pensons de bonne foi que cette divulgation est nécessaire pour protéger nos droits, votre sécurité ou celle d'autrui, enquêter sur une fraude ou répondre à une demande du gouvernement.
+
+    Protection des informations
+    Nous prenons des mesures de sécurité pour protéger les informations personnelles de nos utilisateurs. Cela inclut des mesures de sécurité techniques et organisationnelles pour empêcher l'accès non autorisé, la divulgation, la perte ou la destruction de vos informations personnelles.
+
+    Modifications de la politique de confidentialité
+    Nous pouvons modifier cette politique de confidentialité de temps à autre. Nous vous encourageons à consulter régulièrement cette page pour être informé des changements éventuels.
+
+    Contactez-nous
+    Si vous avez des questions sur cette politique de confidentialité, veuillez nous contacter à l'adresse suivante : service@solescape.com.</p>
+    </div>
+
+    <?php
+        include 'footer.html';
+    ?>
+
+</body>
