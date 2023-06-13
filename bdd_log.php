7f2c9a73884a1365020da1c490f14a77831720f4
nebula <nebula@NEBULABOT>
2023-06-13 21:39:14 +0200

    correction si session deja ouverte

--- bdd_log.php
+++ bdd_log.php
@@ -1,4 +1,8 @@
 <?php
+    // si une session es déjà ouverte, on la ferme
+    if (session_status() == PHP_SESSION_ACTIVE) {
+        session_destroy();
+    }
     //connexion à la base de données
     $db = new PDO('mysql:host=localhost;dbname=solescape;charset=utf8', 'root', '');
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
